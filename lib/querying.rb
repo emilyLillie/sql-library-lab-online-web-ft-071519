9a6d7b9aca8ef9f89badded850a9b3d3460d4256
Emily Lillie <emilylillie3@gmail.com>
2019-08-16 13:15:33 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series INNER JOIN characters ON series.id=characters.series_id GROUP BY series.title ORDER BY SUM
+  "SELECT series.title FROM series INNER JOIN characters ON series.id = characters.series_id GROUP BY series.title ORDER BY SUM
   (characters.species) DESC LIMIT 1"
 end
 
